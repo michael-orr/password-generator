f66e4adcff7e5917d468bea8aed405d4b4ee1dcf
Michael Orr <mike@glquote.com>
2021-09-23 19:50:49 -0700

    got it to generate password but still working on validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,87 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-var charset = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+var specialCharSet = "~!@#$%^&*()";
+var lowerCaseCharSet = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeros = "0123456789";
+var charset = "";
 var passwordLength = 0;
 
 function generatePassword() {
+  //Build character set
+  // special characters prompt
+  specialConfirm = confirm('Do you want to include special characters?');
+  if (specialConfirm === true) {
+    charset += specialCharSet;
+    console.log(charset);
+  };
+    // lowercase prompt
+  lowerConfirm = confirm('Do you want to include lowercase letters?');
+  if (specialConfirm === true) {
+    charset += lowerCaseCharSet;
+    console.log(charset);
+  };
+    // uppercase prompt
+  upperConfirm = confirm('Do you want to include uppercase letters?');
+  if (upperConfirm === true) {
+    charset += upperCaseCharSet;
+    console.log(charset);
+  };
+    // numbers prompt
+    numberConfirm = confirm('Do you want to include numbers?');
+    if (numberConfirm === true) {
+      charset += numeros;
+      console.log(charset);
+    };
+
+
+  //enter password length
   passwordLength = prompt("How long do you want your password?  Enter a number between 8 and 128.");
+  //validate that password length is a number between 8 and 128
   function numberValidation() {
-    if (isNaN(passwordLength)) {
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("Please enter a number between 8 and 128.");
+      console.log('It is returning FALSE for the number validation')
       return false;
     } else {
+      console.log('It is returning true for the number validation')
       return true;
+      
     };
   }
-  function numberRangeValidation() {
-    if (passwordLength < 8) {
-      alert("Please enter a number between 8 and 128.");
-      return false;
-    } else {
-      
-      return true;
-  };
-  }
-  var password = "";
+
   numberValidation();
-  numberRangeValidation();
-    
+  var password = "";
   
+
+
+ if (numberValidation !== false) {
   for (i=0; i < passwordLength; i++) {
     selectChar = charset[Math.floor(Math.random()*charset.length)];
     console.log(selectChar);
     password = (password += selectChar);
     console.log(password);
+    };
+  } else {
+    alert('you suck at this.')
   };
-return password;
+
+  return password;
+
+  
+
+
 }
+
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-  
   passwordText.value = password;
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
